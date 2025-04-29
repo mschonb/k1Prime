d6c5e9e8d825916a83794774eba1c88663b9e266
Marcelo <mschonb@gmail.com>
2025-04-29 16:17:00 -0600

    next app and amplify setup

---
+++ k1prime-core/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "k1primecore": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
